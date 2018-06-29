f4b23beac88880995da1db7c0f04dcaaf30e2f04
NI GUO <guoni0909@gmail.com>
2018-06-29 09:32:39 -0700

    clean up

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -6,11 +6,9 @@ const Card = props => (
 <div className="card">
 {//call the sekectCard function once the player clickes on this img-container
 }
-	<div onClick={ () => {
-			props.selectCard(props.id);
-		}} className="img-container">
-		<img alt={props.name} src={props.image} className="select_card img-fluid"
-		/>
+	<div onClick={ () => { props.selectCard(props.id);	}} 
+		className="img-container">
+	<img alt={props.name} src={props.image} className="select_card img-fluid" />
 	
 	</div>
 </div>
